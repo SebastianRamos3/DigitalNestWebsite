d11cca6bad343776e709c26c563d740e8a74bf13
SebastianRamos3 <seramos@csumb.edu>
2026-06-04 22:37:33 -0700

    Finished most of the functionality, just need js feature and a little more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,56 @@
+import AvatarImage from './Assets/Avatar.jpg'
+import Resume from './Assets/SebRam14.pdf'
+
+import './Styling/App.css'
+import './Styling/back.css'
+
+function Background() {
+  return (
+    <div className="background">
+      {[...Array(8)].map((_, i) => (
+        <div key={i} className="ball" />
+      ))}
+    </div>
+  )
+}
+
+function Profile() {
+  return (
+    <div className="profile">
+      <Avatar />
+
+      <h1 className="name"> Sebastian Ramos</h1>
+      <p className="description">I am a software engineer</p>
+    </div>
+  )
+}
+function Avatar() {
+  return (
+    <img className="avatar" src={AvatarImage} alt="Avatar" />
+  )
+}
+
+function Links({label, url}) {
+  return (
+    <div >
+      <a href={url} target="_blank" rel="noreferrer">{label}</a>
+    </div>
+  )
+}
+
 export default function App() {
-  return null
+
+  return (
+    <>
+      <Background />
+      <div className="app">
+        <Profile />
+          <div className="links">
+            <Links label="LinkedIn" url="https://www.linkedin.com/in/sebastianvramos/" />
+            <Links label="GitHub" url="https://github.com/SebastianRamos3" />
+            <Links label="Resume" url={Resume} />
+          </div>
+      </div>
+    </>
+  )
 }
